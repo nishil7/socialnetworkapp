e361f1a010c562708e25943e8d0ab213b41a473a
nishil7 <nishilbansal7@gmail.com>
2022-07-07 20:21:09 +0530

    New Commit

--- lib/profile.dart
+++ lib/profile.dart
@@ -234,7 +234,7 @@ class _ProfileState extends State<Profile> {
             Padding(
               padding: EdgeInsets.only(top: 20.0),
               child: Text(
-                "No Post Upload",
+                "Nothing to Show",
                 style: TextStyle(
                   color: Colors.redAccent,
                   fontSize: 40.0,
